afbaadaebd14679eae39d4c29c1582463e59e4e1
santihoyos15 <santiagohoyos2808@gmail.com>
2021-09-30 21:50:47 -0500

    Google Search functionality implemented.

---
+++ search.js
@@ -0,0 +1,12 @@
+const input = document.getElementById("search-box"); 
+input.addEventListener("keydown", (event) => {
+    if (event.key == "Enter" && input.value){
+        window.location.href = `https://www.google.com/search?q=${input.value}`;
+    }
+});
+
+const button = document.getElementById("google-search");
+button.addEventListener("click", (event) => {
+
+})
+
